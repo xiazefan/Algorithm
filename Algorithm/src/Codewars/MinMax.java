9bf7624d6c535256287c46ec3b60c69161e71476
George Z Xia <george.z.xia@cnsdc01.pwc.com>
2017-08-29 11:38:45 +0800

    Java API sort

--- Algorithm/src/Codewars/MinMax.java
+++ Algorithm/src/Codewars/MinMax.java
@@ -1,9 +1,20 @@
 package Codewars;
 
+import java.util.Arrays;
+
 public class MinMax {
 
-	public static void main(String[] args) {
+	public static int[] minMax(int[] arr) {
+		Arrays.sort(arr);
+		int result[] = { arr[0], arr[arr.length - 1] };
+		System.out.println(Arrays.toString(result));
+		return arr;
+	}
 
+	public static void main(String[] args) {
+		MinMax mm = new MinMax();
+		int[] a = { 1, 2, 3, 4, 5 };
+		mm.minMax(a);
 	}
 
 }
