c8e80006153025ff8d4e435b1e0a3298e2daa00c
George Z Xia <george.z.xia@cnsdc01.pwc.com>
2017-08-29 17:55:02 +0800

    Bubble

--- Algorithm/src/Codewars/MinMax.java
+++ Algorithm/src/Codewars/MinMax.java
@@ -5,7 +5,16 @@ import java.util.Arrays;
 public class MinMax {
 
 	public static int[] minMax(int[] arr) {
-		Arrays.sort(arr);
+		// Arrays.sort(arr);
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
 		int result[] = { arr[0], arr[arr.length - 1] };
 		System.out.println(Arrays.toString(result));
 		return arr;
@@ -13,7 +22,7 @@ public class MinMax {
 
 	public static void main(String[] args) {
 		MinMax mm = new MinMax();
-		int[] a = { 1, 2, 3, 4, 5 };
+		int[] a = { 5, 4, 3, 2, 1 };
 		mm.minMax(a);
 	}
 
